6f2eb040e4ae1b86d8aa5ae8277dbd4c1ab4e73b
filya27 <fulupushun@gmail.com>
2017-03-14 21:53:08 +0200

    add testCompareAge

--- test/hom3/DogTest.java
+++ test/hom3/DogTest.java
@@ -1,7 +1,5 @@
 package hom3;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,7 +9,14 @@ public class DogTest {
 	public void testCompareName() {
 		Dog dog1 = new Dog("Bobik", Breed.BULLDOG, 12);
 		Dog dog2 = new Dog("Bobik", Breed.GERMAN_SHEPHERD, 2);
-		Assert.assertTrue(dog1.compareName(dog1, dog2));
+		Assert.assertEquals(true, dog1.compareName(dog1, dog2));
+
 	}
 
+	@Test
+	public void testCompareAge() {
+		Dog dog1 = new Dog("Bobik", Breed.BULLDOG, 12);
+		Dog dog2 = new Dog("Bobik", Breed.GERMAN_SHEPHERD, 2);
+		Assert.assertEquals(dog1, dog1.compareAge(dog1, dog2));
+	}
 }
