417d1aafa77f0dbfc009ac149fe34235fb4a54f9
PsP <pspalfa@gmail.com>
2017-03-21 08:56:58 +0200

    HW06 fix

--- HW06/src/hw06/Main.java
+++ HW06/src/hw06/Main.java
@@ -4,24 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		Bird[] birds = new Bird[4];
-//		
-//		birds[0] = new Penguin();
-//		birds[1] = new Chicken();
-//		birds[2] = new Eagle();
-//		birds[3] = new Swallow();
-//		for (Bird bird : birds) {
-//			bird.fly();
-//		}
-//		for (Bird bird : birds) {
-//			if(bird instanceof NonFlyingBird){
-//				bird.setLayEggs(true);
-//			}else{
-//				bird.setLayEggs(false);
-//			}
-//			bird.setFeathers(true);
-//			System.out.println(bird);
-//		}
+		Bird[] birds = new Bird[4];
+		
+		birds[0] = new Penguin();
+		birds[1] = new Chicken();
+		birds[2] = new Eagle();
+		birds[3] = new Swallow();
+		for (Bird bird : birds) {
+			bird.fly();
+		}
+		for (Bird bird : birds) {
+			if(bird instanceof NonFlyingBird){
+				bird.setLayEggs(true);
+			}else{
+				bird.setLayEggs(false);
+			}
+			bird.setFeathers(true);
+			System.out.println(bird);
+		}
 		
 // Task 2
 		Employee[]ems = new Employee[4];
