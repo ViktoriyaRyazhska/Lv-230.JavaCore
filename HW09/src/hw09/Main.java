d4dcef98fd224faba3ca60d43ce29c6909f9139c
PsP <pspalfa@gmail.com>
2017-03-29 22:50:37 +0300

    fix HW09

--- HW09/src/hw09/Main.java
+++ HW09/src/hw09/Main.java
@@ -8,23 +8,28 @@ public class Main {
 
 	public static void main(String[] args) {
 // Task 1
-		System.out.println(div(2, 2));
+		try {
+			
+			System.out.println(div(2, 0));
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 		
 //Task 2
 		System.out.println("Enter number to check");
 		try {
-			System.out.println(readNumber(2, 10));
+			System.out.println(readNumber(-5, 5));
 		} catch (NumberFormatException | IOException | ArithmeticException e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
-	private static double div(double a, double b){
-		if (b != 0){
-			return a/b;
-		}else{
-			throw new ArithmeticException(" div by zero!!!");
+	private static double div(double a, double b)throws ArithmeticException{
+		if (b == 0){
+			throw new ArithmeticException("div by zero!!! \n");
 		}
+			return a/b;
+		
 	}
 	
 	private static int readNumber(int start, int end) throws NumberFormatException, IOException{
@@ -32,9 +37,9 @@ public class Main {
 		int num = Integer.parseInt(br.readLine());
 			if(!(num < end && num > start)){
 				throw new ArithmeticException("Not in range");
-			}else{
-				return num;
 			}
+				return num;
+			
 		
 	}
 }
