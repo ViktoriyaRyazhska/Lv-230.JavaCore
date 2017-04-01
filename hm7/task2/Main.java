721f75d0be16976482321b554e2a72508f374b34
Mykola Malius <mmalyus@mail.ru>
2017-04-01 16:16:41 +0300

    git

---
+++ hm7/task2/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "I    am      learning     Java   Core";
+		System.out.println(a);
+		a = a.replaceAll("\\s+", " ");
+		System.out.println(a);
+		
+		
+
+	}
+
+}
